9ab00ee5238acf82f114d2fb5e3d529e414cd0e4
Lakesh22 <Lakeshkumar19@gmail.com>
2022-12-20 12:47:23 +0530

    completed city repository

--- src/repository/city-repository.js
+++ src/repository/city-repository.js
@@ -1,25 +1,51 @@
-const { City } = require('../models/index');
+const { City } = require("../models/index");
 class CityRepository {
-    async createCity({name}) {
-        try{
-            const city = await City.craete({ name });
-            return city;
-        }catch(error){
-            throw {error};
-        }
+  async createCity({ name }) {
+    try {
+      const city = await City.create({ name }); //by doing { name } we can directly access name without writing obj.name
+      return city;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { error };
     }
+  }
 
-
-async deleteCity(cityId){
-    try{
-        await City.destroy({
-            where:{
-                id:cityId
-            }
-        });
-    }catch(error){
-        throw{error};
+  async deleteCity(cityId) {
+    try {
+      await City.destroy({
+        where: {
+          id: cityId,
+        },
+      });
+      return true;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { error };
     }
-}
+  }
+  async updateCity(cityId, data) {
+    //there are different ways to update
+    try {
+      const city = await City.update(data, {
+        where: {
+          id: cityId,
+        },
+      });
+      return city;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { error };
+    }
+  }
+
+  async getCity(cityId) {
+    try {
+      const city = await City.findByPk(cityId);
+      return city;
+    } catch (error) {
+      console.log("something went wrong in repository layer");
+      throw { error };
+    }
+  }
 }
 module.exports = CityRepository;
